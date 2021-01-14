89de1ce6b87653e2912623ff9d7b149799294fe0
Gibryon Bhojraj <gibryonbhojraj@gmail.com>
2021-01-14 18:58:16 -0500

    Used pug's cache instead of lodash hack.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,9 +7,9 @@ import * as pug from 'pug';
 import * as fs from 'fs';
 import * as util from 'util';
 import Server from '../puppetmaster';
+import { truncate } from 'lodash';
 
 const writeFile = util.promisify(fs.writeFile);
-const f = _.once(pug.compileFile)('src/pdf.pug');
 
 /**
  * firstName
@@ -21,11 +21,17 @@ router.post('/', async (_req, _res, next) => {
     const {firstName, lastName, topicTitle, problems} = _req.body as {firstName: String, lastName: String, topicTitle: String, problems: {number: number, srcdoc: string}[]};
     const filename = `${topicTitle}_${lastName}_${firstName}`;
     
+    // problems.forEach(x => console.log(x.srcdoc));
+
+    const f = pug.compileFile('src/pdf.pug', { filename: 'topic_student_export', cache: true, debug: true });
+
     await writeFile(`/tmp/${filename}.html`, f({
         firstName, lastName, topicTitle, problems
     }), 'utf8');
 
-    await Server.print(filename);
+    console.log(`Wrote /tmp/${filename}.html`);
+
+    // await Server.print(filename);
 
     next(httpResponse.Ok('Test', {}));
 });
